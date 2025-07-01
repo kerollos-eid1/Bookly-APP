6c12d6aa2647920ca925dd286afa26b90c1e1db1
unknown <kerolloseid34@gmail.com>
2025-07-01 23:12:10 +0300

    Added animated container to home screen

--- lib/Features/views/widgets/spalsh_view_body.dart
+++ lib/Features/views/widgets/spalsh_view_body.dart
@@ -1,8 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:listen/Features/views/widgets/sliding_text.dart';
 import 'package:listen/core/photo/assets_photo.dart';
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
+
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+  late Animation<Offset> SlidingAnimation;
+
+  @override
+  void dispose() {
+
+    super.dispose();
+    animationController.dispose();
+  }
+  void initState() {
+    super.initState();
+
+    //لحد هنا كدا انا عملت او جهزت الانيمشن انه هيكون 1 دقيقه
+    animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 1),
+    );
+    // استخدامي ل slidingAnimation دي علشان هيا المتحكمه في عمليه الانيمشن يعني علي الحاجه ال محتاج ليها الانيمشن
+
+    //tween animation =>Responsible for the entire animation process from start to finish.and send to animationcontroller.
+    SlidingAnimation = Tween<Offset>(
+      begin: Offset(0, 5),
+      end: Offset.zero,
+    ).animate(animationController);
+    animationController.forward();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -11,12 +45,9 @@ class SplashViewBody extends StatelessWidget {
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
         Image.asset(AssetsPhoto.Logo),
-      const  Text(
-          ' Read free Books',
-          textAlign: TextAlign.center,
-          style: TextStyle(fontSize: 20),
-        ),
+        SlidingText(SlidingAnimation: SlidingAnimation),
       ],
     );
   }
 }
+
